d8dd25638a57d3c366f621f625a7d2f6164eb2b7
Shaurya Jain <shauryajain19052003@gmail.com>
2026-02-28 03:44:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 
 function createTask(title, assignee) {
-  return { title: title, assignee: assignee };
     return { title: title, assignee: assignee }
 
 function processData(data) {
     const result = []
-for (const item of data) {
+    for (const item of data
         result.push(item)
     return result
 }
 
-app.listen(3000); module.exports = app;
+app.listen(3000)
+module.exports = app
