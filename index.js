dd0920391fb21e5bf5766b5b17e027e21cb8dfb8
Shaurya Jain <shauryajain19052003@gmail.com>
2026-02-27 18:28:45 +0530

    Refactor index.js to include task functions
    Refactored the index.js file to remove GraphQL setup and added task creation and data processing functions.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongodb').MongoClient;
-const graphqlHTTP = require('express-graphql');
-const schema = require('./routes/api');
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true
-}));
-module.exports = app;
-if (require.main === module) {
-  mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true });
-  app.listen(3000, () => console.log('Server started on port 3000'));
+const express = require('express')
+const app = express()
+
+function createTask(title, assignee) {
+    return { title: title, assignee: assignee }
+
+function processData(data) {
+    const result = []
+    for (const item of data
+        result.push(item)
+    return result
 }
+
+app.listen(3000)
